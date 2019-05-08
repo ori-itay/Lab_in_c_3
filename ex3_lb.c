4fe91b3202d1480e51fa59d96ec53c172ed287c5
Origithub <39296766+Origithub@users.noreply.github.com>
2019-05-08 17:31:56 +0300

    No debugging

--- ex3_lb.c
+++ ex3_lb.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/socket.h> // check that all are neccessary
 #include <netinet/in.h>
@@ -11,24 +12,43 @@
 #define MAXIMUM_PORT 64000
 #define STARTING_RANGE_PORT 1024
 #define RECEIVING_BUFFER_SIZE 1024
+#define HTTP_PORT "http_port"
+#define SERVER_PORT "server_port"
+#define END_OF_HTTP_REQUEST "\r\n\r\n"
+#define LENGTH_OF_END_SEQUENCE_OF_HTTP_REQUEST 4
 
-void handle_connections(int *socket);
-void bind_random_port(int* socket ,struct sockaddr_in* server, struct sockaddr_in* http_server);
+void handle_connections(const int *http_socket, int server_sockets_list[3]);
+void bind_random_port(int* http_socket, int* server_socket ,struct sockaddr_in* server_sockaddr, struct sockaddr_in* http_server_sockaddr);
+void write_port_to_file(int random_port, char *port_type);
+int get_random_port();
+char* process_http_request_to_server(char* http_request_buffer, int *server_number, int server_sockets_list[3]);
+void send_back_response_to_client(char* response_from_server, const int* http_socket);
+void connect_with_servers(int *server_socket, int server_sockets_list[3]);
 
 int main(){
-    int listen_socket = -1, opt = 1;
-    struct sockaddr_in server ={0}, http_server = {0};
+    int http_socket = -1, server_socket = -1, opt = 1;
+    struct sockaddr_in server_sockaddr = {0}, http_server_sockaddr = {0};
+    int server_sockets_list[3];
     srand(time(0));
-    
 
-    listen_socket = socket(AF_INET, SOCK_STREAM, 0); // check for failure?
-    setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)); // check for failure?
+    http_socket   = socket(AF_INET, SOCK_STREAM, 0); // check for failure?
+    server_socket = socket(AF_INET, SOCK_STREAM, 0); // check for failure?
+    setsockopt(http_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)); // check for failure?
+    opt = -1; // ?
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)); // check for failure?
 
-    bind_random_port(&listen_socket, &server, &http_server);
-    listen(listen_socket, 10); // check for failure? change from 10 !
-    handle_connections(&listen_socket); // dont care about peer?
+    bind_random_port(&http_socket, &server_socket, &server_sockaddr, &http_server_sockaddr);
+    listen(http_socket, 10); // check for failure? change from 10 !
+    listen(server_socket, 3);
 
-     
+
+
+    connect_with_servers(&server_socket, server_sockets_list);
+
+
+    handle_connections(&http_socket, server_sockets_list); // dont care about peer?
+
+
 
 
 
@@ -39,51 +59,107 @@ int main(){
 }
 
 
-void handle_connections(int *socket){
-    int connection_fd = -1, bytes_read, times_seen_end_of_http_request = 0;
-    char buffer[RECEIVING_BUFFER_SIZE] ={0};
+void connect_with_servers(int *server_socket, int server_sockets_list[3]){
+    server_sockets_list[0] = accept(*server_socket, NULL, NULL);
+    server_sockets_list[1] = accept(*server_socket, NULL, NULL);
+    server_sockets_list[2] = accept(*server_socket, NULL, NULL);
+}
+
+void handle_connections(const int *http_socket, int server_sockets_list[3]){
+    int connection_fd = -1, bytes_read, total_bytes_wrote = 0, server_number = 0;
+    char* end_of_http_request;
+    char buffer[RECEIVING_BUFFER_SIZE] = {0}; char *end_sequence_of_http_request_pointer, *response_from_server;
+    char* http_request_buffer = (char*)calloc(RECEIVING_BUFFER_SIZE, sizeof(char)); // maybe not dynamically?
     while (true){
-        connection_fd = accept(*socket, NULL, NULL);  // check for failure?
-
-        bytes_read = recv(connection_fd, buffer, RECEIVING_BUFFER_SIZE, 0);
-        if (bytes_read == -1){// remove ?
-            printf("bytes read returned -1\n"); // remove ?
-            break;
+        connection_fd = accept(*http_socket, NULL, NULL);  // check for failure?,  TODO: change null to get http info to send back info to it?
+        while (true){
+            bytes_read = recv(connection_fd, buffer, RECEIVING_BUFFER_SIZE, 0);
+            if (bytes_read == -1){// remove ?
+                printf("bytes read returned -1\n"); // remove ?
+                break;
+            }
+            if ((end_sequence_of_http_request_pointer = strstr(buffer, END_OF_HTTP_REQUEST))!=NULL){ // means an end to an http request has come
+                end_of_http_request = end_sequence_of_http_request_pointer + LENGTH_OF_END_SEQUENCE_OF_HTTP_REQUEST;
+                http_request_buffer = memcpy(http_request_buffer + total_bytes_wrote, buffer, end_of_http_request - buffer );
+                printf("%s", http_request_buffer);
+                //total_bytes_wrote  += end_of_http_request_pointer - buffer; // amount of bytes of the current http request
+                response_from_server = process_http_request_to_server(http_request_buffer, &server_number, server_sockets_list);
+                send_back_response_to_client(response_from_server, http_socket);
+                memset(http_request_buffer, 0, RECEIVING_BUFFER_SIZE); free(response_from_server);
+                memcpy(http_request_buffer, end_of_http_request, strlen(end_of_http_request));
+                total_bytes_wrote = strlen(end_of_http_request);
+                continue;
+            }
+            if (bytes_read == 0){ // TODO: check if got the end of http request, if not, continue, else, break ! even before or with no bytes_read==0
+                printf("bytes read returned 0\n");
+                printf("%s", http_request_buffer);
+                break;
+            }
+            http_request_buffer=strncpy(http_request_buffer + total_bytes_wrote, buffer, bytes_read);
+            total_bytes_wrote += bytes_read;
         }
-        printf("%s", buffer);
-        if (bytes_read == 0){
-            printf("bytes read returned 0\n");
 
-            break;
+    }
+    free(http_request_buffer); // doesnt get here
+}
+
+char* process_http_request_to_server(char* http_request_buffer, int *server_number, int server_sockets_list[3]){
+    send(server_sockets_list[*server_number], http_request_buffer, strlen(http_request_buffer), 0);
+
+    char* response_buffer = (char*)calloc(RECEIVING_BUFFER_SIZE, sizeof(char));
+    int bytes_read = recv(server_sockets_list[*server_number], response_buffer, RECEIVING_BUFFER_SIZE, 0); // TODO: this into loop?
+    // TODO: check for if bytes_read==0 etc?
+
+    *server_number = (*server_number + 1) % 3;
+    return response_buffer;
+}
+
+void send_back_response_to_client(char* response_from_server, const int* http_socket){
+
+}
+
+void bind_random_port(int* http_socket, int* server_socket ,struct sockaddr_in* server_sockaddr, struct sockaddr_in* http_server_sockaddr){
+    int random_port;
+    (*http_server_sockaddr).sin_family = AF_INET;
+    (*http_server_sockaddr).sin_addr.s_addr =  INADDR_ANY;
+    (*server_sockaddr).sin_family = AF_INET;
+    (*server_sockaddr).sin_addr.s_addr = INADDR_ANY;     // not sure who initiate the connection..I think LB sends to the servers first, if so, why he must set a listen
+
+    while (true){
+        int random_port = get_random_port();
+        (*http_server_sockaddr).sin_port = htons(random_port);
+        if (bind(*http_socket, (struct sockaddr*)http_server_sockaddr, sizeof(*http_server_sockaddr)) < 0 ){
+            continue;
+        }
+        else {
+            write_port_to_file(random_port, HTTP_PORT);
+            while (true){
+                random_port = get_random_port();
+                if (bind(*server_socket, (struct sockaddr*)server_sockaddr, sizeof(*server_sockaddr))  < 0  ){
+                    continue;
+                }
+                else {
+                    write_port_to_file(random_port, SERVER_PORT);
+                    return;
+                }
+            }
         }
     }
 }
 
+int get_random_port(){
+    int random_port = (rand() % (MAXIMUM_PORT + 1 - STARTING_RANGE_PORT)) + STARTING_RANGE_PORT;
+    return random_port;
+}
 
-void bind_random_port(int* socket ,struct sockaddr_in* server, struct sockaddr_in* http_server){
-    int random_port;
-    (*server).sin_family = AF_INET;
-    (*server).sin_addr.s_addr =  INADDR_ANY;
-
-    while (true){
-        random_port = rand() % (MAXIMUM_PORT+1);
-        if (random_port < STARTING_RANGE_PORT){
-            random_port += STARTING_RANGE_PORT;
-        }
-        // (*server).sin_port = htons(random_port);
-        // if (bind(*socket, (struct sockaddr*)server, sizeof(*server)) < 0 ){
-        //     continue;
-        // }
-
-
-
-        (*server).sin_port = htons(63998); // remove !!
-        if (bind(*socket, (struct sockaddr*)server, sizeof(*server)) < 0 ){ // remove !!
-            printf("Binding to port 80 failed!\n");
-            exit(EXIT_FAILURE);
-        }
-        else {
-            break;
-        }
+void write_port_to_file(int random_port, char *port_type){
+    FILE* fd;
+    if ( strcmp(port_type, HTTP_PORT) ==0 ) {
+        fd = fopen(HTTP_PORT,  "w");
     }
+    else if (strcmp(port_type, SERVER_PORT) == 0) {
+        fd = fopen(SERVER_PORT, "w");
+    }
+    fprintf(fd, "%d", random_port);
+    fclose(fd);
 }
---
+++ ex3_server.py
@@ -0,0 +1,47 @@
+import sys
+from socket import *
+import select
+
+
+
+def main():
+    RECEIVING_SIZE = 1024
+    port = int(sys.argv[1])
+
+
+    server_socket_server_one   = socket(AF_INET, SOCK_STREAM)
+    socket_server_one.connect('localhost', port)
+
+    server_socket_server_two   = socket(AF_INET, SOCK_STREAM)
+    socket_server_two.connect('localhost', port)
+
+    server_socket_server_three = socket(AF_INET, SOCK_STREAM)
+    socket_server_three.connect('localhost', port)
+
+    socket_list = [socket_server_one, socket_server_three, socket_server_three]
+    while True:
+        ready_sockets,_,_ = select.select(socket_list, [], [])
+        for ready_socket in ready_sockets:
+            data_received = ready_socket.recv(RECEIVING_SIZE) # TODO: assumes the whole read is a complete request - maybe its not?
+            response = process_data_received(data_received)
+            ready_socket.send(response)
+
+
+
+
+
+total_counter = 0
+def process_data_received(data_received):
+    get_line_of_address = data_received.split('\r\n')[0]
+    if counter in get_line_of_address:
+        total_counter += 1
+        return """'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length:""" + str(len(str(total_counter))) + """\r\n\r\n""" + str(total_counter) + """\r\n\r\n"""
+    
+    else:
+        return """HTTP/1.1 404 Not Found\r\nContent-type: text/html\r\nContent-length: 113\r\n\r\n<html><head><title>Not Found</title></head><body<\r\nSorry, the object you requested was not found.\r\n</body></html>\r\n\r\n"""
+
+    
+    
+    
+
+
