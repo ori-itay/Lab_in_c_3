a7d8af111b5b6603b15d59de6b7e283c9a6e5510
itaylevi1 <39258073+itaylevi1@users.noreply.github.com>
2019-05-16 05:15:28 -0700

    servers seperated - 1 server for each

--- ex3_server.py
+++ ex3_server.py
@@ -1,4 +1,4 @@
-#!usr/bin/python2.7 -tt
+#! /usr/bin/python2.7 -tt
 import sys
 from socket import *
 import select
@@ -7,41 +7,31 @@ def main():
     RECEIVING_SIZE = 1024
     INIT_COUNT = 0
     port = int(sys.argv[1])
-    servers_request_counter_list = [INIT_COUNT, INIT_COUNT, INIT_COUNT]
+    server_request_counter = [INIT_COUNT]
 
-    server_socket_server_one   = socket()
-    server_socket_server_one.connect(('localhost', port))
+    server_socket = socket()
+    server_socket.connect(('localhost', port))
 
-    server_socket_server_two   = socket()
-    server_socket_server_two.connect(('localhost', port))
-
-    server_socket_server_three = socket()
-    server_socket_server_three.connect(('localhost', port))
-
-    socket_list = [server_socket_server_one, server_socket_server_two, server_socket_server_three]
     while True:
-        ready_sockets,_,_ = select.select(socket_list, [], [])
-        for ready_socket in ready_sockets:
-            data_received = receive_from_ready_socket(ready_socket, RECEIVING_SIZE)
-            response = process_data_received(data_received, servers_request_counter_list, \
-                                             socket_list.index(ready_socket))
-            ready_socket.sendall(response)
+        data_received = receive_from_socket(server_socket, RECEIVING_SIZE)
+        response = process_data_received(data_received, server_request_counter, server_socket)
+        server_socket.sendall(response)
 
-def receive_from_ready_socket(ready_socket, RECEIVING_SIZE):
+def receive_from_socket(server_socket, RECEIVING_SIZE):
     END_OF_HTTP_REQUEST = "\r\n\r\n"
     data_received = ""
     while(END_OF_HTTP_REQUEST not in data_received):
-        data_received+= ready_socket.recv(RECEIVING_SIZE)
+        data_received+= server_socket.recv(RECEIVING_SIZE)
 
     return data_received
 
-def process_data_received(data_received, servers_request_counter_list, current_socket_index):
+def process_data_received(data_received, server_request_counter, server_socket):
     get_line_of_address = data_received.split('\r\n')[0]
     if "counter " in get_line_of_address:
-        servers_request_counter_list[current_socket_index] = servers_request_counter_list[current_socket_index] + 1
+        server_request_counter[0] += 1
         http_response = """HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: """ + \
-                        str(len(str(servers_request_counter_list[current_socket_index]))) + """\r\n\r\n""" + \
-                        str(servers_request_counter_list[current_socket_index]) + """\r\n\r\n"""
+                        str(len(str(server_request_counter[0]))) + """\r\n\r\n""" + \
+                        str(server_request_counter[0]) + """\r\n\r\n"""
     else:
         http_response = """HTTP/1.1 404 Not Found\r\nContent-type: text/html\r\nContent-length: \ 113\r\n\r\n
         <html><head><title>Not Found</title></head><body>\r\n
